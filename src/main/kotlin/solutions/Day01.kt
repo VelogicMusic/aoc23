1f3be5090b1a8eb493fc1d24ea7dd0d2ed674ed0
einSimon <info@simon-einzinger.de>
2023-12-01 15:55:04 +0100

    Solution to day01

---
+++ src/main/kotlin/solutions/Day01.kt
@@ -0,0 +1,33 @@
+package solutions
+
+class Day01(day: Int) : Day(day) {
+    override fun solvePart1(input: String): String = getCalibrationValues(input)
+
+    override fun solvePart2(input: String): String {
+        var transformedInput = input
+        mapOf(
+            "one" to "one1one",
+            "two" to "two2two",
+            "three" to "three3three",
+            "four" to "four4four",
+            "five" to "five5five",
+            "six" to "six6six",
+            "seven" to "seven7seven",
+            "eight" to "eight8eight",
+            "nine" to "nine9nine",
+        ).forEach { (k: String, v: String) ->
+            transformedInput = transformedInput.replace(k, v)
+        }
+        return getCalibrationValues(transformedInput)
+    }
+
+    private fun getCalibrationValues(input: String) =
+        input
+            .lines()
+            .fold(0) { currentSum: Int, line: String ->
+                currentSum +
+                    (
+                        line.first { c -> c.isDigit() }.toString() + line.last { c -> c.isDigit() }.toString()
+                    ).toInt()
+            }.toString()
+}
