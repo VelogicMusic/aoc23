ec9854202d74df29ac87846ba55dca2ca005b8d9
Simon Einzinger <info@simon-einzinger.de>
2023-12-02 12:15:36 +0100

    Added solution day 2

---
+++ src/main/kotlin/solutions/Day02.kt
@@ -0,0 +1,36 @@
+package solutions
+
+class Day02(day: Int) : Day(day) {
+    override fun solvePart1(input: String): String =
+        parseInput(input)
+            .withIndex()
+            .filter { (_, gameSets) ->
+                gameSets.all { colorMap -> 
+                    colorMap.getOrDefault("red", 0) <= 12 && colorMap.getOrDefault("green", 0) <= 13 && colorMap.getOrDefault("blue", 0) <= 14
+                }
+            }
+            .sumOf { (index) -> index + 1 }
+            .toString()
+
+    override fun solvePart2(input: String): String =
+        parseInput(input)
+            .fold(0) { currentSum, gameSets ->
+                currentSum + listOf("red", "green", "blue").fold(1) { accumulator, color ->
+                    accumulator * gameSets.flatMap { listOf(it.getOrDefault(color, 0)) }.max()
+                }
+            }.toString()
+
+    private fun parseInput(input: String): List<List<Map<String, Int>>> =
+        input.lines()
+            .map { game ->
+                game.substringAfter(": ")
+                    .split("; ")
+                    .map { gameSet ->
+                        gameSet
+                            .split(", ")
+                            .associate { colorCount ->
+                                colorCount.split(" ").let { (count, color) -> color to count.toInt() }
+                            }
+                    }
+            }
+}
