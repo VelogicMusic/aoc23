bc4ef93668d6944e7a13c373400bef6c89aa0d35
einSimon <info@simon-einzinger.de>
2023-12-07 16:53:25 +0100

    Added unpolished solution Day07

--- src/main/kotlin/solutions/Day07.kt
+++ src/main/kotlin/solutions/Day07.kt
@@ -1,26 +1,84 @@
 package solutions
 
-data class Hand(val cards: String, val bid: Int): Comparable<Hand> {
+data class HandPart1(val cards: String, val bid: Int) : Comparable<HandPart1> {
     private val strengthMap = "AKQJT98765432".reversed().mapIndexed { index, char -> char to index }.toMap()
 
-    override fun compareTo(other: Hand): Int {
-        val handStrength1 = strengthMap.map { (card, ) -> cards.count { it == card }}.sortedDescending()
-        val handStrength2 = strengthMap.map { (card, _) -> other.cards.count { it == card }}.sortedDescending()
-        return when {
-            handStrength1.first() != handStrength2.first() -> handStrength1.first() - handStrength2.first()
-            handStrength1.first() == 3 && (handStrength1[1] + handStrength2[1] != 4) -> handStrength2[1] - handStrength1[1]
+    private fun getRank(): Int {
+        val freq =
+            strengthMap.map { (card, _) -> cards.count { it == card } }
+                .filter { it != 0 }
+                .sortedDescending()
+        return when (freq) {
+            listOf(5) -> 6
+            listOf(4, 1) -> 5
+            listOf(3, 2) -> 4
+            listOf(3, 1, 1) -> 3
+            listOf(2, 2, 1) -> 2
+            listOf(2, 1, 1, 1) -> 1
+            else -> 0
+        }
+    }
 
-            else ->
-                    cards.zip(other.cards).firstOrNull { (c1, c2) -> c1 != c2 }?.let { (c1, c2) ->
-                        strengthMap[c1]!!.compareTo(strengthMap[c2]!!)
-                    } ?: 0
+    override fun compareTo(other: HandPart1): Int {
+        val thisRank = getRank()
+        val otherRank = other.getRank()
+        return if (thisRank != otherRank) {
+            thisRank - otherRank
+        } else {
+            cards.zip(other.cards).firstOrNull { (c1, c2) -> c1 != c2 }?.let { (c1, c2) ->
+                strengthMap[c1]!!.compareTo(strengthMap[c2]!!)
+            } ?: 0
+        }
+    }
+}
+
+data class HandPart2(val cards: String, val bid: Int) : Comparable<HandPart2> {
+    private val strengthMap = "AKQT98765432J".reversed().mapIndexed { index, char -> char to index }.toMap()
+
+    private fun getRank(): Int {
+        val jokerAmount = cards.count { card -> card == 'J' }
+        val freq =
+            strengthMap.map { (card, _) -> cards.count { it == card } }
+                .filter { it != 0 }
+                .sortedDescending()
+        return when (freq) {
+            listOf(5) -> 6
+            listOf(4, 1) -> if (jokerAmount > 0) 6 else 5
+            listOf(3, 2) -> if (jokerAmount > 0) 6 else 4
+            listOf(3, 1, 1) -> if (jokerAmount > 0) 5 else 3
+            listOf(2, 2, 1) ->
+                when (jokerAmount) {
+                    2 -> 5
+                    1 -> 4
+                    else -> 2
+                }
+            listOf(2, 1, 1, 1) ->
+                when (jokerAmount) {
+                    1, 2 -> 3
+                    else -> 1
+                }
+            else -> jokerAmount
+        }
+    }
+
+    override fun compareTo(other: HandPart2): Int {
+        val thisRank = getRank()
+        val otherRank = other.getRank()
+        return if (thisRank != otherRank) {
+            thisRank - otherRank
+        } else {
+            cards.zip(other.cards).firstOrNull { (c1, c2) -> c1 != c2 }?.let { (c1, c2) ->
+                strengthMap[c1]!!.compareTo(strengthMap[c2]!!)
+            } ?: 0
         }
     }
 }
 
 class Day07(day: Int) : Day(day) {
     override fun solvePart1(input: String): String {
-        val hands = parse(input)
+        val hands =
+            input.lines()
+                .map { line -> line.split(" ").let { (hand, bid) -> HandPart1(hand, bid.toInt()) } }
         return hands.sorted()
             .mapIndexed { index, hand -> (index + 1) * hand.bid }
             .sum()
@@ -28,11 +86,13 @@ class Day07(day: Int) : Day(day) {
     }
 
     override fun solvePart2(input: String): String {
-        return ""
-    }
+        val hands =
+            input.lines()
+                .map { line -> line.split(" ").let { (hand, bid) -> HandPart2(hand, bid.toInt()) } }
 
-    private fun parse(input: String): List<Hand> {
-        return input.lines()
-            .map { line -> line.split(" ").let { (hand, bid) -> Hand(hand, bid.toInt()) } }
+        return hands.sorted()
+            .mapIndexed { index, hand -> (index + 1) * hand.bid }
+            .sum()
+            .toString()
     }
 }
